36662ae02d2d46edb9c341eadc719f81f6847880
orlandobasan <orlandobasan@gmail.com>
2018-09-14 22:46:13 -0400

    Update NO README

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "TESTANDO TRAVIS.")
+        self.assertRegex(result.data.decode(), "testando curso devops Ess")
 
 
 if __name__ == "__main__":
